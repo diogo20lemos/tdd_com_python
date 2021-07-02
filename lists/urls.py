604524eb89366c90ce59953a10e8704ffaf1000e
Cicero Diogo Lemos <diogolemos@EDiagnose.local>
2021-07-02 14:33:36 -0300

    first unit test url and url mapping dummy view

---
+++ lists/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(r'^$', views.home_page, name='home'),
+]
+
+
+
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    # return HttpResponseRedirect('<h1>Ola Mundo</h1>')
+    pass
+
