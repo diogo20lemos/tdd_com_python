120f486457697e6652077b3c80e329d32be66c3c
Cicero Diogo Lemos <diogolemos@EDiagnose.local>
2021-07-05 16:59:30 -0300

    commit antes da refatoracao

--- lists/tests.py
+++ lists/tests.py
@@ -9,7 +9,12 @@ class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'lists/home.html')
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -25,6 +30,8 @@ class HomePageTest(TestCase):
         self.assertTrue(html.endswith('</html>'))
 
         self.assertTemplateUsed(response, 'lists/home.html')
+
+
 
 
 
